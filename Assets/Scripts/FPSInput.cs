b1c59fe20e22f879721c7c78e1dee4dd22315cba
Adam Pridmore <adam_pridmore@hotmail.com>
2021-02-28 21:30:19 +0000

    Add MetaData on required component  - Required CharacterComponent  - Add component name

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -3,26 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FPSInput : MonoBehaviour
-{
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class FPSInput : MonoBehaviour {
     private float speed = 6.0f;
-
     public float gravity = -9.8f;
 
     private CharacterController _characterController;
 
-    void Start()
-    {
+    void Start() {
         _characterController = GetComponent<CharacterController>();
     }
     
-    void Update()
-    {
+    void Update() {
         float deltaX = Input.GetAxis("Horizontal") * speed ;
         float deltaZ = Input.GetAxis("Vertical") * speed;
 
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-
         movement = Vector3.ClampMagnitude(movement, speed);
         movement.y = gravity;
 

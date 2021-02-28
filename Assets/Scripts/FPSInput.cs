33c9a225de2e95489cf604b6f4e53c03eb562e0a
Adam Pridmore <adam_pridmore@hotmail.com>
2021-02-28 21:21:42 +0000

    Use CharacterControler for applying movement  - 2.5.3

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -6,12 +6,26 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     private float speed = 6.0f;
-    // Update is called once per frame
+
+    private CharacterController _characterController;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+    
     void Update()
     {
         float deltaX = Input.GetAxis("Horizontal") * speed ;
         float deltaZ = Input.GetAxis("Vertical") * speed;
-        
-        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        _characterController.Move(movement);
     }
 }

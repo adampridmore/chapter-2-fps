4abde53297b2c6456af757283a075e1a4601c50c
Adam Pridmore <adam_pridmore@hotmail.com>
2021-02-28 21:27:01 +0000

    Apply gravity to player Split:   MouseLook to y for player object   MouseLoot to x for camera object

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -7,6 +7,8 @@ public class FPSInput : MonoBehaviour
 {
     private float speed = 6.0f;
 
+    public float gravity = -9.8f;
+
     private CharacterController _characterController;
 
     void Start()
@@ -22,6 +24,7 @@ public class FPSInput : MonoBehaviour
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
 
         movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
 
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
